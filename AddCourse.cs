264902f49f1092ddf468e68b7e38e89cf7fd5202
kthanush18 <kthanush.18@gmail.com>
2021-07-18 16:06:26 +0530

    Base code

---
+++ AddCourse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using efcoredemo.Model;
+using efCoredemo.Model;
+
+namespace efCoredemo
+{
+    class AddCourse
+    {
+        static void Main(string[] args)
+        {
+            var ctx = new CatalogContext();
+            var course = new Course { Title = "Python", Prereq = "Basic Programming" };
+            ctx.Courses.Add(course);
+            ctx.SaveChanges();
+        }
+    }
+}
---
+++ Model/Course.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace efCoredemo.Model
+{
+    public class Course
+    {
+        public int Id
+        {
+            get; set;
+        }
+
+        public string Title
+        {
+            get; set;
+        }
+        public string Prereq
+        {
+            get; set;
+        }
+        public int Fee { get; set; }
+    }
+}
